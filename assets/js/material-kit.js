219b98435edb94d212f5cef7130b1aba65a7717d
SamArtGS <samuelarturo@icloud.com>
2019-12-24 09:43:38 -0600

    Cambio Material Kit Pro 1.2.1

--- assets/js/material-kit.js
+++ assets/js/material-kit.js
@@ -331,8 +331,8 @@ var big_image;
      },
 
      initContactUs2Map: function(){
-         var lat = 44.433530;
-         var long = 26.093928;
+         var lat = 19.482501;
+         var long = -98.867369;
 
          var centerLong = long - 0.025;
 
@@ -342,15 +342,13 @@ var big_image;
          var mapOptions = {
            zoom: 14,
            center: centerPosition,
-           styles:
- [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}],
            scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
          }
          var map = new google.maps.Map(document.getElementById("contactUs2Map"), mapOptions);
 
          var marker = new google.maps.Marker({
              position: myLatlng,
-             title:"Hello World!"
+             title:"Clínica SL Huexotla"
          });
          marker.setMap(map);
      }
