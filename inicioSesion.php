a662416dddc3b164969a5ff28e47186e2881f5bc
SamArtGS <samuelarturo@icloud.com>
2019-12-29 21:18:47 -0600

    Enlazado a AWS

--- inicioSesion.php
+++ inicioSesion.php
@@ -1,25 +1,28 @@
 <?php
-$mysqli = new mysqli("127.0.0.1", "root", "", "mydb", 3306);
 
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL";
+$enlace = mysqli_connect("slh.chjrd0648elz.us-west-2.rds.amazonaws.com", "proteco", "proteco123", "clinicaslh");
+
+if (!$enlace) {
+        echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+        echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+        echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+        exit;
 }
 
 $login      = $_POST['usuario'];
 $contrasena = $_POST['contrasena'];
 
-$sql = "SELECT Contrasena FROM medico WHERE CedProf=$login";
+$sql = "SELECT Contrasena FROM MEDICO WHERE CedProf=$login";
 
 
-$result = mysqli_query($mysqli, $sql);
+$result = mysqli_query($enlace, $sql);
 $row = mysqli_fetch_array($result);
-
 
 if (md5($contrasena) == $row[0]) {
   session_start();
   $_SESSION['medico'] = $login;
   header("Location: ListaPaciente.php");
-  exit(); 
+  exit();
 }
 else {
   header("Location: IniciarSesion.php");
